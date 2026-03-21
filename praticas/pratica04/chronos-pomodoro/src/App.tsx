969efef186035446384a97d716a3acd5ee21527a
carolinaarosendo <carolinaarosendo@gmail.com>
2026-03-20 21:46:57 -0300

    Finalizando a Pratica07

---
+++ praticas/pratica04/chronos-pomodoro/src/App.tsx
@@ -0,0 +1,14 @@
+import "./styles/theme.css";
+import "./styles/globals.css";
+import { Heading } from "./components/Heading";
+import { Timer } from 'lucide-react'; // Importando o ícone
+export function App(){
+    return (
+    <Heading>
+      Histórico
+      <button>
+        <Timer /> {/* Renderizando o ícone dentro do botão */}
+      </button>
+    </Heading>
+  );
+}
