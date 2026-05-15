bb555b26c7ab7d6611930a29c970c97b5c63f16a
carolinaarosendo <carolinaarosendo@gmail.com>
2026-05-15 17:37:59 -0300

    Organizando o caminho do Login

--- praticas/Pomodoro/src/routers/MainRouter/index.tsx
+++ praticas/Pomodoro/src/routers/MainRouter/index.tsx
@@ -2,9 +2,10 @@ import { BrowserRouter, Route, Routes, useLocation } from 'react-router';
 import { AboutPomodoro } from '../../pages/AboutPomodoro';
 import { NotFound } from '../../pages/NotFound';
 import { Home } from '../../pages/Home';
+import { Login } from '../../pages/Login'; // 1. Certifique-se de que o import do seu Login está correto aqui!
 import { useEffect } from 'react';
 import { History } from '../../pages/History';
-import { Settings } from '../../pages/Settings'; // Novo import!
+import { Settings } from '../../pages/Settings';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -20,9 +21,15 @@ export function MainRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* 2. AGORA O LOGIN É A PORTA DE ENTRADA DO APP */}
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        
+        {/* 3. A HOME FOI MOVIDA PARA SEU PRÓPRIO CAMINHO */}
+        <Route path="/home" element={<Home />} />
+        
         <Route path="/history/" element={<History />} />
-        <Route path="/settings/" element={<Settings />} /> {/* Rota conectada! */}
+        <Route path="/settings/" element={<Settings />} />
         <Route path="/about-pomodoro/" element={<AboutPomodoro />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
